8d7335d32224a236b4c0e51aa70250acdf353d3d
Vamsi6296 <56160821+Vamsi6296@users.noreply.github.com>
2019-11-01 11:10:27 +0530

    my first file
    added a new file

---
+++ HelloWorld.java
@@ -0,0 +1,22 @@
+/*First create a source program.
+Save the source program with FileName.java
+Compile the Program
+Execute the Program.*/
+
+public class  HelloWorld
+{
+	public static void main(String[] args)	
+	{
+		System.out.println("Hello World!");
+	}
+}
+
+public class Demo
+{
+	public static void main(String[] args)	
+	{
+		System.out.println("From Demo");
+	}	
+}
+
+
